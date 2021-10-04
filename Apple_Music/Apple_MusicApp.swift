b2f3e3cfa05fb87ecc02262813cf206a792bc307
a.vladimirovich@internet.ru <163Fork>
2021-10-04 13:56:30 +0400

    Initial Commit

---
+++ Apple_Music/Apple_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_MusicApp.swift
+//  Apple_Music
+//
+//  Created by Артем on 04.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct Apple_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

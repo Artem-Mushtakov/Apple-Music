469e4b06ce19038d1b9b90cb96c5d35b8e752db0
a.vladimirovich@internet.ru <163Fork>
2021-10-04 14:20:57 +0400

    Added view hierarchy

---
+++ Apple_Music/View/MediaLibraryView.swift
@@ -0,0 +1,20 @@
+//
+//  MediaLibraryView.swift
+//  Apple_Music
+//
+//  Created by Артем on 04.10.2021.
+//
+
+import SwiftUI
+
+struct MediaLibraryView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MediaLibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        MediaLibraryView()
+    }
+}
